380e458e4416f3307644bf19831cd7f0c3e41aac
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2019-01-10 19:49:51 +1100

    Create swerve modules and add rescale_js

--- robot.py
+++ robot.py
@@ -1,13 +1,50 @@
 #!/usr/bin/env python3
 
+import ctre
 import magicbot
 import wpilib
 
+from pyswervedrive.chassis import Chassis
+from pyswervedrive.module import SwerveModule
+from utilities.functions import rescale_js
+
 
 class Robot(magicbot.MagicRobot):
+
+    chassis: Chassis
+
     def createObjects(self):
         """Create motors and stuff here."""
-        pass
+        self.module_a = SwerveModule(  # top left module
+            "a",
+            steer_motor=ctre.TalonSRX(1),
+            drive_motor=ctre.TalonSRX(2),
+            x_pos=0.25,  # TODO: update correct x and y pos
+            y_pos=0.31,
+        )
+        self.module_b = SwerveModule(  # bottom left module
+            "b",
+            steer_motor=ctre.TalonSRX(3),
+            drive_motor=ctre.TalonSRX(4),
+            x_pos=-0.25,  # TODO: update correct x and y pos
+            y_pos=0.31,
+        )
+        self.module_c = SwerveModule(  # bottom right module
+            "c",
+            steer_motor=ctre.TalonSRX(5),
+            drive_motor=ctre.TalonSRX(6),
+            x_pos=-0.25,  # TODO: update correct x and y pos
+            y_pos=-0.31,
+        )
+        self.module_d = SwerveModule(  # top right module
+            "d",
+            steer_motor=ctre.TalonSRX(7),
+            drive_motor=ctre.TalonSRX(8),
+            x_pos=0.25,  # TODO: update correct x and y pos
+            y_pos=-0.31,
+        )
+        self.joystick = wpilib.Joystick(0)
+        self.gamepad = wpilib.XboxController(1)
 
     def teleopInit(self):
         """Initialise driver control."""
@@ -15,7 +52,23 @@ class Robot(magicbot.MagicRobot):
 
     def teleopPeriodic(self):
         """Allow the drivers to control the robot."""
-        pass
+        throttle = (1 - self.joystick.getThrottle()) / 2
+
+        joystick_vx = -rescale_js(
+            self.joystick.getY(), deadzone=0.1, exponential=1.5, rate=4 * throttle
+        )
+        joystick_vy = -rescale_js(
+            self.joystick.getX(), deadzone=0.1, exponential=1.5, rate=4 * throttle
+        )
+        joystick_vz = -rescale_js(
+            self.joystick.getZ(), deadzone=0.2, exponential=20.0, rate=self.spin_rate
+        )
+        self.chassis.set_inputs(
+            joystick_vx,
+            joystick_vy,
+            joystick_vz,
+            field_oriented=not self.joystick.getRawButton(6),
+        )
 
 
 if __name__ == "__main__":
---
+++ utilities/functions.py
@@ -0,0 +1,28 @@
+import math
+
+
+def rescale_js(
+    value, deadzone: float = 0.0, exponential: float = 0.0, rate: float = 1.0
+):
+    """Rescale a joystick input, applying a deadzone, exponential, and max rate.
+
+    Args:
+        value: the joystick value, in the interval [-1, 1].
+        deadzone: the deadzone to apply.
+        exponential: the strength of the exponential to apply
+                     (i.e. how non linear should the response be)
+        rate: the max rate to return (ie the value to be returned when 1.0 is given)
+    """
+    sign = 1
+    if value < 0:
+        sign = -1
+        value = -value
+    # Apply deadzone
+    if value < deadzone:
+        return 0.0
+    if not exponential:
+        value = (value - deadzone) / (1 - deadzone)
+    else:
+        a = math.log(exponential + 1) / (1 - deadzone)
+        value = (math.exp(a * (value - deadzone)) - 1) / exponential
+    return rate * sign * value
